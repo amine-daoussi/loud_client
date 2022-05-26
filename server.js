6a3065d67228aeab8e081d30a6712a3f5c397d4c
Walid Msallem <walidmsallem@gmail.com>
2022-05-26 19:19:06 +0100

    w

--- server.js
+++ server.js
@@ -61,7 +61,9 @@ app.post('mail/forma', (req, res) => {
 // app.use(compression());
 app.use('/', express.static(path.resolve(process.cwd(), 'build')));
 
-app.use('/*', (req, res) => {
+// app.use('/*', (req, res) => {
+// jareb hedhy
+app.use('*', (req, res) => {
   // res.sendFile(path.join(__dirname, 'build', 'index.html'));
 
   res.sendFile(path.resolve(path.resolve(process.cwd(), 'build'), 'index.html'));
